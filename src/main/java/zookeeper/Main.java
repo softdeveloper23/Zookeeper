2be6723ceeb222f17b46830bd855823d8310d8cb
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-05 15:16:30 +0100

    Added code to print out 5 lines of text

--- src/main/java/zookeeper/Main.java
+++ src/main/java/zookeeper/Main.java
@@ -1,5 +1,11 @@
 package zookeeper;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
 }
