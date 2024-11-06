6590fde6e31de2124f0b90efa2c8fb7a33be260a
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-06 18:31:12 +0100

    ÂPrint an image of an animal

--- src/main/java/zookeeper/Main.java
+++ src/main/java/zookeeper/Main.java
@@ -2,10 +2,27 @@ package zookeeper;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I love animals!");
-        System.out.println("Let's check on the animals...");
-        System.out.println("The deer looks fine.");
-        System.out.println("The bat looks happy.");
-        System.out.println("The lion looks healthy.");
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
+        System.out.println(camel);
     }
 }
